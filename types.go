798b575284d6f7a357a920beca96e244ecb98b19
Gleb Tv <glebtv@gmail.com>
2020-06-17 00:32:02 +0300

    add Data to type

--- types.go
+++ types.go
@@ -1,8 +1,13 @@
 package slate_to_text
 
 type Node struct {
-	Children []Node
-	Type     *string
-	Text     *string
-	Code     *bool
+	Children      []Node
+	Type          *string
+	Text          *string
+	Code          *bool
+	Italic        *bool
+	Bold          *bool
+	Underline     *bool
+	Strikethrough *bool
+	Data          map[string]interface{} `json:"data,omitempty"`
 }
